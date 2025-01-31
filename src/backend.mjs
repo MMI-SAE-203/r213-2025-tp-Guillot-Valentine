9a958f733705a1bb0173cfedba539cba74f1bfc5
Guillot-Valentine <valentine.guillot@edu.univ-fcomte.fr>
2025-01-31 14:17:58 +0100

    pocketbase et image

---
+++ src/backend.mjs
@@ -0,0 +1,19 @@
+import PocketBase from 'pocketbase';
+
+const pb = new PocketBase('http://127.0.0.1:8090');
+
+export async function getOffres() {
+    try {
+        let data = await pb.collection('Maison').getFullList({
+            sort: '-created',
+        });
+        data = data.map((ev) => {
+            ev.img= pb.files.getURL(ev, ev.photo_maison);
+            return ev;
+        });
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en lisant la liste des maisons', error);
+        return [];
+    }
+}
