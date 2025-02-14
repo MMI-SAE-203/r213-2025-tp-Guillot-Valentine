e119f9078d1e4f70670e181445afd3580df36149
Guillot-Valentine <valentine.guillot@edu.univ-fcomte.fr>
2025-02-14 14:00:36 +0100

    ajouter une offre

--- src/backend.mjs
+++ src/backend.mjs
@@ -50,3 +50,19 @@ export async function getOffresByPrice(p) {
         });
     return prix;
 }
+
+export async function addOffre(house) {
+    try {
+        await pb.collection('Maison').create(house);
+        return {
+            success: true,
+            message: 'Offre ajoutée avec succès'
+        };
+    } catch (error) {
+        console.log('Une erreur est survenue en ajoutant la maison', error);
+        return {
+            success: false,
+            message: 'Une erreur est survenue en ajoutant la maison'
+        };
+    }
+}
